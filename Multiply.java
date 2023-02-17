414ea8f50f150bda48631da20b67738f6e9edca8
Prasad Raju <prasadraju3443@gmail.com>
2023-02-17 20:35:19 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+public class Multiply {
+
+    public static void main(String args[]){
+
+        
+        int a=3,b=3;
+        System.out.println("Product: " + (a*b));
+  
+  
+  
+      }
+    
+}
